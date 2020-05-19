e3228d6f041e3d57d8677c3be463a36793986e41
Amir <ahsafari@sfu.ca>
2020-05-19 02:17:38 -0700

    complete data analyzer

--- Data analyze.py
+++ Data analyze.py
@@ -11,12 +11,44 @@ def LoadLabel(path):
 
 df_train, dt = data_preprocess.process(2, 0)
 
-# dt = LoadLabel('Data/')
+dfPyrazinamide = dt[0]
 
-# print(dt)
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("streptomycin")
+print(one)
+print(zero)
+print(nan)
+
+
+dfPyrazinamide = dt[1]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("rifampicin")
+print(one)
+print(zero)
+print(nan)
 
 dfPyrazinamide = dt[2]
-# print(dfPyrazinamide)
 
 arr = dfPyrazinamide.values.tolist()
 zero = 0
@@ -34,46 +66,113 @@ print(one)
 print(zero)
 print(nan)
 
+dfPyrazinamide = dt[3]
 
-# dfPyrazinamide = dt[['moxifloxacin']]
-# # print(dfPyrazinamide)
-#
-# arr = dfPyrazinamide.values.tolist()
-# zero = 0
-# one = 0
-# nan = 0
-# for i in range(0, len(arr)):
-#     if arr[i][0] == 1:
-#         one = one + 1
-#     elif arr[i][0] == 0:
-#         zero = zero + 1
-#     else:
-#         nan = nan + 1
-# print("moxifloxacin")
-# print(one)
-# print(zero)
-# print(nan)
-#
-# dfPyrazinamide = dt[['streptomycin']]
-# # print(dfPyrazinamide)
-#
-# arr = dfPyrazinamide.values.tolist()
-# zero = 0
-# one = 0
-# nan = 0
-# for i in range(0, len(arr)):
-#     if arr[i][0] == 1:
-#         one = one + 1
-#     elif arr[i][0] == 0:
-#         zero = zero + 1
-#     else:
-#         nan = nan + 1
-# print("streptomycin")
-# print(one)
-# print(zero)
-# print(nan)
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("ofloxacin")
+print(one)
+print(zero)
+print(nan)
 
+dfPyrazinamide = dt[4]
 
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("moxifloxacin")
+print(one)
+print(zero)
+print(nan)
+
+dfPyrazinamide = dt[5]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("kanamycin")
+print(one)
+print(zero)
+print(nan)
+
+dfPyrazinamide = dt[6]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("isoniazid")
+print(one)
+print(zero)
+print(nan)
+
+dfPyrazinamide = dt[7]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("ethionamide")
+print(one)
+print(zero)
+print(nan)
+
+dfPyrazinamide = dt[8]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("ethambutol")
+print(one)
+print(zero)
+print(nan)
 
 dfPyrazinamide = dt[9]
 
@@ -93,8 +192,7 @@ print(one)
 print(zero)
 print(nan)
 
-
-dfPyrazinamide = dt[7]
+dfPyrazinamide = dt[10]
 
 arr = dfPyrazinamide.values.tolist()
 zero = 0
@@ -107,7 +205,26 @@ for i in range(0, len(arr)):
         zero = zero + 1
     else:
         nan = nan + 1
-print("ethionamide")
+print("capreomycin")
 print(one)
 print(zero)
 print(nan)
+
+dfPyrazinamide = dt[11]
+
+arr = dfPyrazinamide.values.tolist()
+zero = 0
+one = 0
+nan = 0
+for i in range(0, len(arr)):
+    if arr[i][0] == 1:
+        one = one + 1
+    elif arr[i][0] == 0:
+        zero = zero + 1
+    else:
+        nan = nan + 1
+print("amikacin")
+print(one)
+print(zero)
+print(nan)
+
