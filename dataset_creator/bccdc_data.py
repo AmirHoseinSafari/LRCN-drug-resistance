7ed1c5dce9d3dcfdc0058a278129e7b288d1b813
Amir <ahsafari@sfu.ca>
2021-01-16 13:44:17 -0800

    add bccdc data

---
+++ dataset_creator/bccdc_data.py
@@ -0,0 +1,148 @@
+import csv
+import pandas as pd
+
+
+
+# isolate = ['A', 'B', 'A']
+# print(type(isolate))
+# print(type(isolate[0]))
+# iso_dict = dict()
+# for i in range(0, len(isolate)):
+#     if isolate[i] in iso_dict:
+#         index_pointer = int(iso_dict[isolate[i]])
+#     else:
+#         iso_dict.update({isolate[i]: str(152)})
+#
+# print(iso_dict)
+# graph={'A':['B','C'],
+#    'B':['C','D']}
+#
+# print('A' in graph)
+
+
+def load_gene_positions():
+    dt = pd.read_csv('../Data/EPFL_Data/Mycobacterium_tuberculosis_H37Rv_allGenes.csv')
+    dt.set_index(dt.columns[0], inplace=True, drop=True)
+
+    start = sum(dt[['Start']].values.tolist(),[])
+    stop = sum(dt[['Stop']].values.tolist(), [])
+    # start = start[0:3981]
+    # stop = stop[0:3981]
+    # dt = pd.read_csv('../Data/EPFL_Data/Mycobacterium_tuberculosis_H37Rv_genes_v3.csv')
+    # dt.set_index(dt.columns[0], inplace=True, drop=True)
+    #
+    # start.extend(sum(dt[['Start']].values.tolist(), []))
+    # stop.extend(sum(dt[['Stop']].values.tolist(), []))
+    return start, stop
+
+
+def binary_search(start, stop, low, high, x):
+    # Check base case
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        # If element is present at the middle itself
+        if start[mid] <= x <= stop[mid]:
+            return mid
+
+            # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif start[mid] > x:
+            return binary_search(start, stop, low, mid - 1, x)
+
+            # Else the element can only be present in right subarray
+        else:
+            return binary_search(start, stop, mid + 1, high, x)
+
+    else:
+        # Element is not present in the array
+        return -1
+
+
+tsv_file = open("../Data/bccdc_snippy_179_snps_training_data.tsv")
+read_tsv = csv.reader(tsv_file, delimiter="\t")
+
+isolate = []
+position = []
+
+for row in read_tsv:
+    isolate.append(row[0])
+    position.append(row[4])
+
+isolate = isolate[1:]
+position = position[1:]
+
+for i in range (0, len(position)):
+    position[i] = int(position[i])
+
+start, stop = load_gene_positions()
+
+zeros = []
+
+res = []
+
+for i in range(0, len(start)):
+    zeros.append(0)
+
+
+iso_dict = dict()
+for i in range(0, len(isolate)):
+    if i % 100 == 0:
+        print(i)
+
+    index_pointer = -1
+
+    if isolate[i] in iso_dict:
+        index_pointer = int(iso_dict[isolate[i]])
+    else:
+        tmp = [isolate[i]]
+        tmp.extend(zeros)
+        res.append(tmp)
+        iso_dict.update({isolate[i]: str(len(res) - 1)})
+        index_pointer = len(res) - 1
+
+    # if len(res) == 0:
+    #     tmp = [isolate[i]]
+    #     tmp.extend(zeros)
+    #     res.append(tmp)
+    #     index_pointer = 0
+    # else:
+    #     for j in range(0, len(res)):
+    #         if res[j][0] == isolate[i]:
+    #             index_pointer = j
+    #             break
+    #     if index_pointer == -1:
+    #         tmp = [isolate[i]]
+    #         tmp.extend(zeros)
+    #         res.append(tmp)
+    #         index_pointer = len(res) - 1
+
+    gene_index = binary_search(start, stop, 0, len(start) - 1, position[i])
+
+    if gene_index != -1:
+        res[index_pointer][gene_index + 1] = res[index_pointer][gene_index + 1] + 1
+
+    # found_gene = -1
+    # for j in range(0, len(start)):
+    #     passed = 0
+    #     if start[j] <= position[i] <= stop[j]:
+    #         # print("1111111111111")
+    #         # print(j)
+    #         # print(len(res[index_pointer]))
+    #         res[index_pointer][j+1] = res[index_pointer][j+1] + 1
+    #         break
+    #     if start[j] > position[i]:
+    #         passed = 1
+    #     if passed == 1 and position[i] > stop[j]:
+    #         break SRR6153157
+
+    f = open('bccdc_data.csv', 'w')
+    for item in res:
+        for i in range(len(item)):
+            if i == 0:
+                f.write(str(item[i]))
+            else:
+                f.write(',' + str(item[i]))
+        f.write('\n')
+    f.close()
