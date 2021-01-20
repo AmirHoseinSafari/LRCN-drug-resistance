f844b9f3882cd1f02a8be80c179e9df7054b9593
Amir <ahsafari@sfu.ca>
2021-01-19 16:36:47 -0800

    gbt on snp

--- pipe_line_gene.py
+++ pipe_line_gene.py
@@ -8,15 +8,15 @@ epochs = 200
 
 
 def train():
-    df_train, labels = data_preprocess.process(38, gene_dataset=True)
+    # df_train, labels = data_preprocess.process(38, gene_dataset=True)
     # gene_dataset_creator.main()
-    # df_train, labels = data_preprocess.process(2)
+    df_train, labels = data_preprocess.process(38)
     # source_analysis.main(df_train)
     # df_train, labels = data_preprocess.process(38, shuffle_operon_group=True)
-    # ML_methods.model_run(df_train, labels)
+    ML_methods.model_run(df_train, labels)
     # model_gene_based.run_model(df_train, labels, epochs)
     # wide_n_deep.run_bayesian(df_train, labels)
-    model_gene_based.run_bayesian(df_train, labels)
+    # model_gene_based.run_bayesian(df_train, labels)
     # model_gene_based.run_all(df_train, labels, epochs)
     # model_gene_based.run_model_kfold(df_train,labels,epochs)
 
